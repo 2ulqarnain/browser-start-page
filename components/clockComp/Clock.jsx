9238323bb9104e4b137ee8619eb7b8ae6d0f4f22
zulqernayn <zulqernaynh@gmail.com>
2023-02-02 11:01:18 +0500

    redesigned a bit

--- components/clockComp/Clock.jsx
+++ components/clockComp/Clock.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react"
+import {Kanit} from "@next/font/google"
+
+const fred_font = Kanit({
+  subsets: ["latin"],
+  weight:"400"
+});
 
 export default function Clock() {
-    const [time,setTime]=useState(new Date()) 
+    const [time,setTime]=useState(new Date())
 
     const refreshClock=()=>setTime(new Date())
+
 
     useEffect(()=>{
         const intervalId=setInterval(refreshClock,1000)
@@ -11,9 +18,10 @@ export default function Clock() {
     },[])
     
   return (
-    <div className='text-center'>
-        <h4 className='text-9xl text-green-500'>{time.getHours()}</h4>
-        <h4 className='text-9xl text-blue-500'>{time.getMinutes().toString().padStart(2,0)}</h4>
+    <div className={`text-center ${fred_font.className}`}>
+        <h4 className='text-9xl text-green-500'>{time.toLocaleTimeString('en-uk',{hour:"numeric"})}</h4>
+        <h4 className='text-9xl text-blue-500'>{time.toLocaleTimeString('en-uk',{minute:"numeric"})}</h4>
+        <h4 className='text-2xl text-zinc-400'>{time.toLocaleString('en-uk',{dateStyle:"full"})}</h4>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from 'next/head'
 import Clock from '../components/clockComp/Clock'
+import {Sacramento} from "@next/font/google"
+
+const font =  Sacramento({
+  subsets: ["latin"],
+  weight:"400"
+});
 
 export default function Home() {
   return (
@@ -12,7 +18,9 @@ export default function Home() {
       <main className='w-full h-full font-ware gap-y-10 flex flex-col justify-center items-center'>
         <Clock/>
         <div>
-          <h4 className='text-5xl text-zinc-800 mynamehover'>Zulqarnain Haider</h4>
+          <h4 className={`text-transparent p-2 text-4xl bg-clip-text bg-gradient-to-r from-green-400 to-blue-600 ${font.className}`}>
+            zulqarnain haider
+          </h4>
         </div>
       </main>
 

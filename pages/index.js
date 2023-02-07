badb616fd21c0e6fb480372a68a3091a2221b5cb
zulqernayn <zulqernaynh@gmail.com>
2023-02-07 20:41:40 +0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -14,16 +14,9 @@ export default function Home() {
         <title>Home Page</title>
         <meta name="description" content="home page for browsers" />
       </Head>
-
       <main className='w-full h-full font-ware gap-y-10 flex flex-col justify-center items-center'>
         <Clock/>
-        <div>
-          <h4 className={`text-transparent p-2 text-4xl bg-clip-text bg-gradient-to-r from-green-400 to-blue-600 ${font.className}`}>
-            zulqarnain haider
-          </h4>
-        </div>
       </main>
-
     </div>
   )
 }

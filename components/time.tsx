474b560ad1f2b19cad58d00dd31d8e0d151fcd4f
Zulqarnain Haider <zulqernaynh@gmail.com>
2023-05-07 20:30:31 +0500

    moved randomization in ccomp in usememo

--- components/time.tsx
+++ components/time.tsx
@@ -1,19 +1,31 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 type Props = {
   className?: string,
-  primaryColorClass:string,
-  secondaryColorClass:string
 };
 
 const returnTime=()=>(new Date()
         .toLocaleString("en-us", { timeStyle: "short", hour12: false }))
         .split(":")
 
-export default function Time({ className,primaryColorClass,secondaryColorClass }: Props) {
+export default function Time({ className }: Props) {
     const [time,setTime] = useState(returnTime())
+    const tailwindColors = [
+        'text-gray-400',
+        'text-red-400',
+        'text-orange-400',
+        'text-yellow-400',
+        'text-green-400',
+        'text-teal-400',
+        'text-blue-400',
+        'text-indigo-400',
+        'text-purple-400',
+        'text-pink-400'
+      ];
+    
+      const [primaryColorClass,secondaryColorClass]=useMemo(()=>[tailwindColors[Math.floor(Math.random()*10)],tailwindColors[Math.floor(Math.random()*10)]],[])
     useEffect(()=>{
         const timeIntervalId=setInterval(()=>setTime(returnTime()),1000)
         return ()=>clearInterval(timeIntervalId)

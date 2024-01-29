b665b9f25008d7189cac6a840ffd5e5f34334cfb
Zulqarnain Haider <zulqernaynh@gmail.com>
2024-01-29 15:35:54 +0500

    fixed timezone and added realtime digital clock

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import type { Viewport } from 'next'
+import Time from "@/components/time";
 
 export const viewport: Viewport = {
     themeColor: 'white',
@@ -6,18 +7,11 @@ export const viewport: Viewport = {
 
 export default function Home() {
 
-    const timeNow = new Date()
-        .toLocaleTimeString("en-us",{
-            hour:"numeric",
-            minute:"numeric",
-            hour12:false
-        })
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white text-black text-3xl font-trochut">
       <div className={"text-center p-2 rounded-2xl"}>
           <p>Zulqarnain Haider</p>
-          <p className={"text-8xl"}>{timeNow}</p>
+          <Time/>
       </div>
     </main>
   );
--- components/time.tsx
+++ components/time.tsx
@@ -1,39 +1,16 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
-type Props = {
-  className?: string,
-};
+const returnTime=()=>new Date().toLocaleTimeString("en-us", { timeStyle: "short", hour12: false })
 
-const returnTime=()=>(new Date()
-        .toLocaleString("en-us", { timeStyle: "short", hour12: false }))
-        .split(":")
-
-export default function Time({ className }: Props) {
+export default function Time() {
     const [time,setTime] = useState(returnTime())
-    const tailwindColors = [
-        'text-gray-400',
-        'text-red-400',
-        'text-orange-400',
-        'text-yellow-400',
-        'text-green-400',
-        'text-teal-400',
-        'text-blue-400',
-        'text-indigo-400',
-        'text-purple-400',
-        'text-pink-400'
-      ];
-    
-      const [primaryColorClass,secondaryColorClass]=useMemo(()=>[tailwindColors[Math.floor(Math.random()*10)],tailwindColors[Math.floor(Math.random()*10)]],[])
     useEffect(()=>{
         const timeIntervalId=setInterval(()=>setTime(returnTime()),1000)
         return ()=>clearInterval(timeIntervalId)
     },[])
     return (
-        <div className="font-monoton text-center text-8xl">
-            <p className={primaryColorClass}>{time[0]}</p>
-            <p className={secondaryColorClass}>{time[1]}</p>
-        </div>
+        <p className={"text-8xl"}>{time}</p>
     );
 }
